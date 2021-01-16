6d80ca6e3485a11d0a4a91bc4ecaeed6a84a1b0d
Rory Gao <30483987+biosharp18@users.noreply.github.com>
2021-01-15 23:39:07 -0500

    added blank view

--- server.js
+++ server.js
@@ -11,4 +11,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static('frontend'))
+
+app.post('/submit', (req, res) => {
+    //console.log(req.body) //you will get your data in this as object.
+    res.cookie("context", req.body.name, { httpOnly: true });
+    res.redirect(req.body.room)
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
