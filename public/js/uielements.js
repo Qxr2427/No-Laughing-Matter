83a44a7b04944e0206c5d8890bd47d2689c99ee5
qxr2427 <qiuxinrong27@gmail.com>
2021-01-17 01:37:16 -0500

    Updated progress bar algorithm

--- public/js/uielements.js
+++ public/js/uielements.js
@@ -1,7 +1,13 @@
 const progressBar = document.getElementById("progress-bar");
 var scores = [20, 20, 20, 20]
 var prevScore = 90
-var prevWidth = 90
+var prevWidth = 80
+var scoreRecords = []
+for (var i = 0; i < 300 ; i++){
+  scoreRecords.push(80)
+}
+
+
 socket.on("update_score", (data) => {
   //console.log(Math.round(data.score));
   scores.shift()
@@ -11,26 +17,43 @@ socket.on("update_score", (data) => {
   for (i = 0; i < scores.length; i++){
     avg += scores[i]
   }
+  var curMax = Math.max(...scoreRecords)
   var curScore = avg/scores.length
-  if ((curScore < 50) || (curScore < prevScore)){
-    //console.log(curScore + " " + prevScore)
+  if(Number.isNaN(curScore)){
+    scoreRecords.shift()
+    scoreRecords.push(50)
+    curScore = 50
+    curMax = 100
+  }
+  else{
+    if (curScore > curMax){
+    scoreRecords.shift()
+    scoreRecords.push(curScore)
+    }
+    else{
+    scoreRecords.shift()
+    scoreRecords.push(curMax - 20)
+    }
+  }
+  if ((curScore < 60) || (curScore < prevScore) || (curScore < curMax)){
+    console.log("Case 1: " + prevWidth)
+    console.log(curScore + " " + curMax)
     prevScore = curScore
-    prevWidth = prevWidth - 1
-   //console.log("Case 1: " + prevWidth)
+    prevWidth = prevWidth - 0.1
     progressBar.style=  `width: ${Math.round(prevWidth)}%`
     
   }
   else {
-    var diff = curScore - prevScore
-    //console.log(curScore + " " + prevScore)
+    var diff = curScore - curMax
     var curWidth = prevWidth + diff
+    console.log("Case 2: " + curWidth)
+    console.log(curScore + " " + curMax)
     prevScore = curScore
     progressBar.style=  `width: ${Math.round(curWidth)}%`
-    //console.log("Case 2: " + curWidth)
     prevWidth = curWidth
   }
-  console.log(`width: ${progressBar.style.width}`)
-  console.log(`max: ${progressBar.getAttribute('aria-valuemax')}`)
+  //console.log(`width: ${progressBar.style.width}`)
+  //console.log(`max: ${progressBar.getAttribute('aria-valuemax')}`)
   let percentage = parseFloat(progressBar.style.width) / 100.0;
   const minHue = 80
   const maxHue = 235
@@ -41,9 +64,9 @@ socket.on("update_score", (data) => {
     progressBar.style.backgroundColor = `rgb(235, ${hue}, 52)`
   }
 
-  console.log(`background-color: hsl(${hue}, 100%, 100%)`)
-  console.log(progressBar.style.backgroundColor)
-  console.log("recieve cur_score");
+  //console.log(`background-color: hsl(${hue}, 100%, 100%)`)
+  //console.log(progressBar.style.backgroundColor)
+  //console.log("recieve cur_score");
 });
 
 
