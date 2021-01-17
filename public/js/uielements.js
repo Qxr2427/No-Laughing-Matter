10f97ea0fad5f5c343860ed7e1eee099e3ef7f03
Alan Wang <alan.wang9018@gmail.com>
2021-01-17 08:36:59 -0500

    listening for progress bar below 0

--- public/js/uielements.js
+++ public/js/uielements.js
@@ -1,75 +1,71 @@
 const progressBar = document.getElementById("progress-bar");
-var scores = [20, 20, 20, 20]
-var prevScore = 90
-var prevWidth = 80
-var scoreRecords = []
-for (var i = 0; i < 300 ; i++){
-  scoreRecords.push(80)
+var scores = [20, 20, 20, 20];
+var prevScore = 90;
+var prevWidth = 80;
+var curWidth;
+var scoreRecords = [];
+for (var i = 0; i < 300; i++) {
+  scoreRecords.push(80);
 }
-
 
 socket.on("update_score", (data) => {
   //console.log(Math.round(data.score));
-  socket.on('check-score', data=>{
-    //code to check if score reaches below threshold
-    //show turn over dialogue, prompt next turn
-  })
-  scores.shift()
-  scores.push(Math.round(data.score))
-  var avg = 0
-  var i
-  for (i = 0; i < scores.length; i++){
-    avg += scores[i]
-  }
-  var curMax = Math.max(...scoreRecords)
-  var curScore = avg/scores.length
-  if(Number.isNaN(curScore)){
-    scoreRecords.shift()
-    scoreRecords.push(50)
-    curScore = 50
-    curMax = 100
-  }
-  else{
-    if (curScore > curMax){
-    scoreRecords.shift()
-    scoreRecords.push(curScore)
-    }
-    else{
-    scoreRecords.shift()
-    scoreRecords.push(curMax - 20)
-    }
-  }
-  if ((curScore < 60) || (curScore < prevScore) || (curScore < curMax)){
-    console.log("Case 1: " + prevWidth)
-    console.log(curScore + " " + curMax)
-    prevScore = curScore
-    prevWidth = prevWidth - 0.1
-    progressBar.style=  `width: ${Math.round(prevWidth)}%`
+  socket.on("check-score", (data) => {
+
     
+  });
+  scores.shift();
+  scores.push(Math.round(data.score));
+  var avg = 0;
+  var i;
+  for (i = 0; i < scores.length; i++) {
+    avg += scores[i];
   }
-  else {
-    var diff = curScore - curMax
-    var curWidth = prevWidth + diff
-    console.log("Case 2: " + curWidth)
-    console.log(curScore + " " + curMax)
-    prevScore = curScore
-    progressBar.style=  `width: ${Math.round(curWidth)}%`
-    prevWidth = curWidth
+  var curMax = Math.max(...scoreRecords);
+  var curScore = avg / scores.length;
+  if (Number.isNaN(curScore)) {
+    scoreRecords.shift();
+    scoreRecords.push(50);
+    curScore = 50;
+    curMax = 100;
+  } else {
+    if (curScore > curMax) {
+      scoreRecords.shift();
+      scoreRecords.push(curScore);
+    } else {
+      scoreRecords.shift();
+      scoreRecords.push(curMax - 20);
+    }
+  }
+
+  if (curScore < 60 || curScore < prevScore || curScore < curMax) {
+    console.log("Case 1: " + prevWidth);
+    console.log(curScore + " " + curMax);
+    prevScore = curScore;
+    prevWidth = prevWidth - 0.1;
+    progressBar.style = `width: ${Math.round(prevWidth)}%`;
+  } else {
+    var diff = curScore - curMax;
+    curWidth = prevWidth + diff;
+    console.log("Case 2: " + curWidth);
+    console.log(curScore + " " + curMax);
+    prevScore = curScore;
+    progressBar.style = `width: ${Math.round(curWidth)}%`;
+    prevWidth = curWidth;
   }
   //console.log(`width: ${progressBar.style.width}`)
   //console.log(`max: ${progressBar.getAttribute('aria-valuemax')}`)
   let percentage = parseFloat(progressBar.style.width) / 100.0;
-  const minHue = 80
-  const maxHue = 235
-  let hue = Math.round(minHue + (percentage * (maxHue - minHue)))
+  const minHue = 80;
+  const maxHue = 235;
+  let hue = Math.round(minHue + percentage * (maxHue - minHue));
 
-  if (!Number.isNaN(hue)){
+  if (!Number.isNaN(hue)) {
     // progressBar.style.backgroundColor = `hsl(` + hue + `, 100%, 100%)`
-    progressBar.style.backgroundColor = `rgb(235, ${hue}, 52)`
+    progressBar.style.backgroundColor = `rgb(235, ${hue}, 52)`;
   }
 
   //console.log(`background-color: hsl(${hue}, 100%, 100%)`)
   //console.log(progressBar.style.backgroundColor)
   //console.log("recieve cur_score");
 });
-
